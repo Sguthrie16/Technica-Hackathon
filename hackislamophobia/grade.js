2fec948c5f217ce182d2310b794401eb7fa8385d
Sarah Guthrie <sarah.guthrie16@stjohns.edu>
2020-10-24 22:24:07 -0400

    Checking Score

--- hackislamophobia/grade.js
+++ hackislamophobia/grade.js
@@ -27,10 +27,8 @@ answer ="";
       document.getElementById("start").style.display ="none";
       current_question_one = data[count];
       number_of_questions_one = Object.keys(data).length;
-      this.question_type_one = current_question_one.question_type;
       this.question_id_one = current_question_one.id;
       this.answer = current_question_one.correct_answers;
-    console.log(current_question_one);
     generate_quiz_one();
 }
 
@@ -38,8 +36,6 @@ answer ="";
 
 
 function generate_quiz_one(){
-  console.log(current_question_one);
-  if(question_type_one == "Multiple choice"){
     document.getElementById("quiz1").innerHTML = `
     <form>
     <h2>${current_question_one.question}</h2>
@@ -52,45 +48,25 @@ function generate_quiz_one(){
       <input type="radio" value = ${current_question_one.options.optiond} name=${current_question_one.options.optiond} onclick= "score_tracker(this.value)">
       <label>${current_question_one.options.optiond}</label><br>
     </form>`
-  }
-  else if (question_type_one == "Multiple selection") {
-    document.getElementById("quiz1").innerHTML = `
-    <form>
-        <h2><b>${current_question_one.question}</b></h2>
-        <input type="checkbox" class="answers btn btn-default" name="vehicle1" value="${current_question_one.options.optiona}">
-        <label for="vehicle1">${current_question_one.options.optiona}</label><br>
-        <input type="checkbox" class="answers btn btn-default" name="vehicle2" value="${current_question_one.options.optionb}">
-        <label for="vehicle2"> ${current_question_one.options.optionb}</label><br>
-        <input type="checkbox" class="answers btn btn-default" name="vehicle3" value="${current_question_one.options.optionc}">
-        <label for="vehicle3"> ${current_question_one.options.optionc}</label><br>
-        <input type="checkbox" class="answers btn btn-default" name="vehicle3" value="${current_question_one.options.optiond}">
-        <label for="vehicle3"> ${current_question_one.options.optiond}</label><br>
-        <button type="button" onclick="compairing_answers()" class="btn btn-default">Submit</button>
-      </form>`
-}
-
   return false;
 }
-function compairing_answers(){
-var inputElements = document.getElementsByClassName('answers');
-checkedValue = "";
-for(var i=0; inputElements[i]; i++){
-      if(inputElements[i].checked){
-           checkedValue +=inputElements[i].value;
-      }
-}
-score_tracker(checkedValue);
-return false;
-}
+
 
 function score_tracker(selected_answer){
-  console.log(selected_answer);
-  console.log(answer);
   if (answer == selected_answer) {
       current_score = current_score + 1;
+      get_api_data('https://my-json-server.typicode.com/sguthrie16/Technica-Hackathon/Questions_Quiz');
         }
     count = count +1;
-    console.log(current_score);
-  get_api_data('https://my-json-server.typicode.com/sguthrie16/Technica-Hackathon/Questions_Quiz');
+    console.log(count);
+    console.log(number_of_questions_one);
+  if(count == number_of_questions_one){
+    complete();
+  }
+
+
 return false;
+}
+function complete(){
+  console.log(current_score);
 }
